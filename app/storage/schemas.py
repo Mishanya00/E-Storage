91216dbac68fd200d2465acfe07498efb1e823df
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2025-05-11 23:17:42 +0300

    File pydantic schema

---
+++ app/storage/schemas.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+
+
+class FileSchema(BaseModel):
+    file_id: Annotated[int, Field(ge=0)]
+    user_id: Annotated[int, Field(ge=0)]
+    filename: str
+    path: str
+    size: int
+    mime_type: str
