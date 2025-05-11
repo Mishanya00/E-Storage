38b2150ecf1e0ce2c6fe2e92e9e312ce7cd4a706
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2025-05-11 16:02:50 +0300

    File upload added. Empty files bug

--- app/storage/routes.py
+++ app/storage/routes.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, BackgroundTasks, UploadFile, File, HTTPException,
 
 from app.auth.dependencies import get_current_user
 from app.auth.schemas import UserSchema
+from app.storage.service import background_save_file
 
 
 storage_router = APIRouter()
@@ -19,11 +20,15 @@ async def create_upload_file(background_tasks: BackgroundTasks,
          raise HTTPException(status_code=400, detail="File has no filename")
     else:
         try:
-            pass
+            background_tasks.add_task(
+                background_save_file,
+                upload_file=file,
+                email=curr_user.email
+            )
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e)) from e
         finally:
             pass
             # await file.close()
 
-        return {"message": f"file {file.filename} has been successfully uploaded."}
+        return {"message": f"file {file.filename} has been added to upload queue."}
--- app/storage/service.py
+++ app/storage/service.py
@@ -1,5 +1,7 @@
-import os
+import shutil
 from pathlib import Path
+
+from fastapi import UploadFile
 
 
 UPLOAD_DIR = Path('/app/storage/uploads') # os.getcwd() returns /app
@@ -7,5 +9,19 @@ UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
 
 def create_email_folder(email: str):
-    email_upload_dir = UPLOAD_DIR / email
-    email_upload_dir.mkdir(exist_ok=True)
+    user_dir = UPLOAD_DIR / email
+    user_dir.mkdir(exist_ok=True)
+
+
+def background_save_file(upload_file: UploadFile, email: str):
+    """Wrapper for file-saving (for additional functionality in future)"""
+    save_uploaded_file(upload_file, email)
+
+
+def save_uploaded_file(upload_file: UploadFile, email: str) -> Path:
+    file_path = UPLOAD_DIR / email / upload_file.filename
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(upload_file.file, buffer)
+
+    # return file_path
